1c8765d8dca739e25c0686f775f087541a21afbb
realCoderSoo <sooem082@gmail.com>
2021-04-14 09:07:00 -0400

    Terrain added

---
+++ Assets/Provided/StartMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        StartCoroutine(ChangeLevel());
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator ChangeLevel() {
+        GameObject menuController = GameObject.Find("MenuController");
+        float time = menuController.GetComponent<FaderScript>().BeginFade(1);
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(1);
+    }
+}
